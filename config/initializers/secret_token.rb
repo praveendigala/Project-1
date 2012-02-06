34c15e886e19b41367ac9572a1410d899fb29aa3
praveen digala <pravin.digala@gmail.com>
2012-02-05 19:08:53 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsinstallerDemo::Application.config.secret_token = '7b518d88a5fb77070e26899484027c8aa20ac8972aaf97fe1d189207d5e1a7131b7bc626db6379516e4c5f2617758906a711a13a67cd6e3b6ba63899e3689a88'
---
+++ test/functional/generate_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class GenerateControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
